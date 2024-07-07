93b22c17c6bc7a7285eeb6969f1a68aaefbf0966
Cytan2000 <tanchiyong00@gmail.com>
2024-07-07 18:09:59 +0800

    [FEAT] rearrange image

--- frontend-tiktok-AI/app/layout.tsx
+++ frontend-tiktok-AI/app/layout.tsx
@@ -2,6 +2,7 @@ import { Toaster } from "@/components/ui/sonner";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ImgProvider } from "./imgContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children}
-        <Toaster />
+        <ImgProvider>
+          {children}
+          <Toaster />
+        </ImgProvider>
       </body>
     </html>
   );
--- frontend-tiktok-AI/components/TikTokLoginButton.tsx
+++ frontend-tiktok-AI/components/TikTokLoginButton.tsx
@@ -1,21 +1,22 @@
 // components/TikTokLoginButton.js
 "use client";
 
-import React from 'react';
-
 const TikTokLoginButton = () => {
-  const CLIENT_KEY = 'sbawgbcd6kwg0yhkmk';
-  const REDIRECT_URI = 'http://localhost:3000/callback';
+  const CLIENT_KEY = "sbawgbcd6kwg0yhkmk";
+  const REDIRECT_URI = "http://localhost:3000/callback";
 
   const handleLogin = () => {
     const state = Math.random().toString(36).substring(2);
-    const authUrl = `https://www.tiktok.com/auth/authorize/?client_key=${CLIENT_KEY}&response_type=code&scope=user.info.basic&redirect_uri=${encodeURIComponent('http://localhost:3000/callback')}&state=${state}`;
-  window.location.href = authUrl;
+    const authUrl = `https://www.tiktok.com/auth/authorize/?client_key=${CLIENT_KEY}&response_type=code&scope=user.info.basic&redirect_uri=${encodeURIComponent("http://localhost:3000/callback")}&state=${state}`;
+    window.location.href = authUrl;
     window.location.href = authUrl;
   };
 
   return (
-    <button onClick={handleLogin} className="mt-4 p-2 bg-blue-500 text-white rounded">
+    <button
+      onClick={handleLogin}
+      className="mt-4 rounded bg-blue-500 p-2 text-white"
+    >
       Login with TikTok
     </button>
   );
