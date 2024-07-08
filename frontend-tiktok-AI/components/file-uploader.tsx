b9ae6430253a4df2b8847df33a464549c808fbbd
Cytan2000 <tanchiyong00@gmail.com>
2024-07-08 10:16:54 +0800

    [FIX] display image from LLM

--- frontend-tiktok-AI/components/file-uploader.tsx
+++ frontend-tiktok-AI/components/file-uploader.tsx
@@ -32,7 +32,7 @@ export default function FileUploader() {
 
     const additionalDictionary = {
       type: "text",
-      text: "I am creating a TikTok slideshow post. I have a list of images that I want to arrange meaningfully to tell a story and generate captions. Feel free to rearrange the images and highlight details such as main objects, time of day, and locations. Please return the rearranged images in JSON format. Here's an example format: { 'details':{'main_objects':[],'time_of_day':'','location':'','caption':''},'original_position': 0}, do not return me any other text, i want it in a valid JSON string , do not give me extra ```",
+      text: "I am creating a TikTok slideshow post. I have a list of images that I want to arrange meaningfully to tell a story and generate captions. Feel free to rearrange the images and highlight details such as main objects, time of day, and locations, original position is the index of the image starting from 0. Please return the rearranged images in JSON format. Here's an example format: { 'details':{'main_objects':[],'time_of_day':'','location':'','caption':''},'original_position': 0}, do not return me any other text, i want it in a valid JSON string , do not give me extra ```",
     };
 
     // Prepend the additional dictionary
